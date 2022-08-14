f6466f5acdbae72d0d1e78e074d627f3918e23ef
Romit Tiwari <romittiwari100@gmail.com>
2022-08-14 18:24:08 +0530

    Backend Done

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
-const multer = require("multer");
+
 router.post("/", async (req, res) => {
   const newCat = new Category(req.body);
   try {
