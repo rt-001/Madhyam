83c58440d2a0211467a375f46c46105c36575f6c
Romit Tiwari <romittiwari100@gmail.com>
2022-08-24 17:18:19 +0530

    API INTEGRATION DONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,28 @@
-import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
+import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useContext } from "react";
+import { Context } from "./context/Context";
 function App() {
-  const currentUser = false;
+  const { user } = useContext(Context);
+  // console.log(currentUser);
+  // console.log("user");
   return (
     <BrowserRouter>
       <Topbar></Topbar>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/posts" element={<Homepage />} />
-        <Route
-          path="/register"
-          element={currentUser ? <Homepage /> : <Register />}
-        />
-        <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
+        <Route path="/register" element={user ? <Homepage /> : <Register />} />
+        <Route path="/login" element={user ? <Homepage /> : <Login />} />
         <Route path="/post/:id" element={<Single />} />
-        <Route path="/write" element={currentUser ? <Write /> : <Login />} />
-        <Route
-          path="/settings"
-          element={currentUser ? <Settings /> : <Login />}
-        />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -1,15 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./topbar.css";
 
-export default function Topbar() {
-  const user = true;
+export default function TopBar() {
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:8800/images/";
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
   return (
     <div className="top">
       <div className="topLeft">
         <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
       </div>
       <div className="topCenter">
         <ul className="topList">
@@ -18,24 +25,30 @@ export default function Topbar() {
               HOME
             </Link>
           </li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
           <li className="topListItem">
             <Link className="link" to="/write">
               WRITE
             </Link>
           </li>
-          {user && <li className="topListItem">LOGOUT</li>}
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className="topRight">
         {user ? (
-          <Link className="link" to="/settings">
-            <img
-              className="topImg"
-              src="https://images.unsplash.com/photo-1587691592099-24045742c181?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
-              alt=""
-            />
+          <Link to="/settings">
+            <img className="topImg" src={PF + user.profilePic} alt="" />
           </Link>
         ) : (
           <ul className="topList">
